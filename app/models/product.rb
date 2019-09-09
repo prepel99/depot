c9131deeacffff9481cddad6ae2447fb8fde2bc1
Bohdan Prepelytsia <bprep@macmini4564.softservecom.com>
2019-09-09 18:45:01 +0300

    started making a cart

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,6 @@
 class Product < ApplicationRecord
+  has_many :line_items
+  before_destroy :ensure_not_referenced_by_any_line_item
   validates :title, :description, :image_url,presence: true
   validates :price, numericality: {greater_than_or_equal_to:  0.01}
   validates :image_url, allow_blank: true, format: {
@@ -6,4 +8,12 @@ class Product < ApplicationRecord
       message: 'must be a URL for GIF,JPG or PNG image.'
   }
   validates :title, uniqueness: true
+
+  private
+  def ensure_not_referenced_by_any_line_item
+    unless line_items.empty?
+      errors.add(:base, 'Line Items present')
+      throw :abort
+    end
+  end
 end
